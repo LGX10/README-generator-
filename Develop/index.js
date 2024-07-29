c3759ae70319b808175aedd68075fed8f0a39a0a
CRbukoski <camerorb45@gmail.com>
2024-07-29 14:22:26 -0400

    added prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,58 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = inquirer.prompt = ([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Write your description here!',
+    }
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Write your install instructions here!',
+    }
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Write your usage instructions here!',
+    }
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Write your credits here!',
+    }
+
+    {
+        type: 'list',
+        name: 'title',
+        message: 'Select a liscense!',
+        name: 'liscense',
+        choices: ['red', 'blue', 'green', 'yellow', 'cyan', 'magenta'],
+    }
+
+    {
+      
+
+    }
+    // Add more questions here
+]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
